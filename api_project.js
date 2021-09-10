66616c5893ba3f77e375fea2c648ba5cb197db61
HugoMeyer <48103842+HugoMeyer@users.noreply.github.com>
2021-09-10 15:57:06 +0200

    Add files via upload

--- api_project.js
+++ api_project.js
@@ -1,16 +1,16 @@
 define({
-  "name": "AREA",
+  "name": "Hynos",
   "version": "0.1.0",
-  "description": "apiDoc AREA",
-  "url": "http://localhost:8080/",
+  "description": "apiDoc Hynos",
+  "url": "http://localhost:3000/",
   "useHostUrlAsSampleUrl": false,
-  "sampleUrl": "http://localhost:8080/",
+  "sampleUrl": "http://localhost:3000/",
   "apidoc": "0.3.0",
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-06T21:15:44.391Z",
+    "time": "2021-09-10T13:54:00.921Z",
     "url": "https://apidocjs.com",
-    "version": "0.26.0"
+    "version": "0.29.0"
   }
 });
